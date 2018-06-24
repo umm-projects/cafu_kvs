eb4c08550be27779604e2cdc4b2b915c12423f05
mattak <mattak.me@gmail.com>
2018-06-25 01:00:13 +0900

    feat/support_json_net :seedling: IStringDataSerializer implemented

---
+++ Assets/Scripts/Utility/JsonUtilitySerializer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CAFU.KeyValueStore.Utility
+{
+    public class JsonUtilitySerializer : IStringDataSerializer
+    {
+        public string Serialize<TValue>(TValue value)
+        {
+            return JsonUtility.ToJson(value);
+        }
+
+        public TValue Deserialize<TValue>(string text)
+        {
+            return JsonUtility.FromJson<TValue>(text);
+        }
+    }
+}
