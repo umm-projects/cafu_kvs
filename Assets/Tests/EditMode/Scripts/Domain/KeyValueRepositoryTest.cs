712821619f8c5593391c8832bde77397ccdea665
Tetsuya Mori <monry84@gmail.com>
2018-07-23 11:28:01 +0900

    :white_check_mark: Add test

---
+++ Assets/Tests/EditMode/Scripts/Domain/KeyValueRepositoryTest.cs
@@ -0,0 +1,106 @@
+using System;
+using CAFU.Core.Data.Entity;
+using CAFU.KeyValueStore.Domain.Repository;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.EditMode.Scripts.Domain
+{
+    public class KeyValueRepositoryTest
+    {
+        private static readonly string DataPath = Application.persistentDataPath + "/test.kv";
+
+        private IKeyValueRepository KeyValueRepository { get; set; }
+
+        [Serializable]
+        private class SampleEntity : IEntity
+        {
+            [SerializeField] private int id;
+
+            public int Id
+            {
+                get { return id; }
+                set { id = value; }
+            }
+
+            [SerializeField] private string name;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public SampleEntity()
+            {
+            }
+
+            public SampleEntity(int id, string name) : this()
+            {
+                Id = id;
+                Name = name;
+            }
+        }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            KeyValueRepository = new DefaultKeyValueRepository.Factory().Create(DataPath);
+            KeyValueRepository.Load();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ((DefaultKeyValueRepository)KeyValueRepository).DataStore.Clear();
+        }
+
+        [Test]
+        public void GetOrCreateEntityTest()
+        {
+            {
+                var entity = KeyValueRepository.GetEntity<SampleEntity>("key");
+                Assert.IsNull(entity);
+            }
+
+            {
+                KeyValueRepository.Load();
+                var entity = KeyValueRepository.GetOrCreateEntity<SampleEntity>("key");
+                Assert.IsNotNull(entity);
+                Assert.AreEqual(0, entity.Id);
+                Assert.AreEqual(null, entity.Name);
+
+                entity.Id = 1;
+                entity.Name = "foo";
+                Assert.AreEqual(1, entity.Id);
+                Assert.AreEqual("foo", entity.Name);
+
+                // Save without SetEntity
+                KeyValueRepository.Save();
+            }
+
+            {
+                KeyValueRepository.Load();
+                var entity = KeyValueRepository.GetEntity<SampleEntity>("key");
+                Assert.IsNotNull(entity);
+                Assert.AreEqual(0, entity.Id);
+                // Will be empty string when saved
+                Assert.AreEqual(string.Empty, entity.Name);
+                entity.Id = 1;
+                entity.Name = "foo";
+
+                KeyValueRepository.SetEntity("key", entity);
+                KeyValueRepository.Save();
+            }
+
+            {
+                KeyValueRepository.Load();
+                var entity = KeyValueRepository.GetEntity<SampleEntity>("key");
+                Assert.IsNotNull(entity);
+                Assert.AreEqual(1, entity.Id);
+                Assert.AreEqual("foo", entity.Name);
+            }
+        }
+    }
+}
